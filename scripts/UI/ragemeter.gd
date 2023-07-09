b1b06084a7037088c143d8d5ac36e8835b78b038
Rishit Khare <kharerishit@gmail.com>
2023-07-08 20:14:46 -0700

    ragemeter flash white

--- scripts/UI/ragemeter.gd
+++ scripts/UI/ragemeter.gd
@@ -7,6 +7,8 @@ extends Control
 func _process(delta):
 	smoothed += 0.1 * (value - smoothed)
 	prog_bar_node.value = smoothed + 0.004
+	var mod = 1 + 20 * abs(value - smoothed)
+	prog_bar_node.modulate = Color(mod, mod, mod)
 
 func set_rage_value(val : float):
 	value = val
