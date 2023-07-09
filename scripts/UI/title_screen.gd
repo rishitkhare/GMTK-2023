23ba9c381e8a9076f3e081e40c933abc5f97e789
Rishit Khare <kharerishit@gmail.com>
2023-07-09 07:15:45 -0700

    use for instead of repetitive code in title screen

--- scripts/UI/title_screen.gd
+++ scripts/UI/title_screen.gd
@@ -4,8 +4,6 @@ extends TextureRect
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# hide game UI on title screen
-	UI.get_node("RageMeter").hide()
-	UI.get_node("DriverControls").hide()
-	
-	UI.get_node("RageMeter").set_process_input(false)
-	UI.get_node("DriverControls").set_process_input(false)
+	for ui_node in UI.get_children():
+		ui_node.hide()
+		ui_node.set_process_input(false)
